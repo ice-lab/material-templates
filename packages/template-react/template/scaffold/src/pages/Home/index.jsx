284485bf4b97ec5cdd9273e00859a498d5147e65
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2020-02-27 00:43:57 +0800

    feat: template-react 升级 icejs

--- packages/template-react/template/scaffold/src/pages/Home/index.jsx
+++ packages/template-react/template/scaffold/src/pages/Home/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Guide from '@/components/Guide';
 
 export default function Home() {
   return (
     <div>
-      <h2>Home Page</h2>
-      <Link to="/">Go to about page</Link>
+      <Guide />
     </div>
   );
 }
--- packages/template-react/template/scaffold/src/pages/NotFound/index.jsx
+++ packages/template-react/template/scaffold/src/pages/NotFound/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'ice';
 import styles from './index.module.scss';
 
 export default function NotFound() {
