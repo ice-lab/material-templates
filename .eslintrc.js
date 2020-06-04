0901e2f3c028b390a1ccf3f160e6e9095d2c5063
yangfan <18767120422@163.com>
2020-06-04 17:59:14 +0800

    fix: lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ const { eslintTS, deepmerge } = require('@ice/spec');
 
 module.exports = deepmerge(eslintTS, {
   rules: {
+    "react/react-in-jsx-scope": 0,
     "@typescript-eslint/no-var-requires": 0,
     "class-methods-use-this": 1,
   }
