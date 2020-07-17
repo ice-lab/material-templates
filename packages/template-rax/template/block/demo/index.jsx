9976da25e50f9d30800117db89d6193b508903db
yangfan <18767120422@163.com>
2020-07-17 11:39:18 +0800

    feat: update block

--- packages/template-rax/template/block/demo/index.jsx
+++
@@ -1,5 +0,0 @@
-import { createElement, render } from 'rax';
-import DriverUniversal from 'driver-universal';
-import Block from '../src/index';
-
-render(<Block />, document.body, { driver: DriverUniversal });
