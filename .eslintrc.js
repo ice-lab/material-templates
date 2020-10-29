fc4f8ad0925a56873340986f7c4cef89afae2505
yangfan <18767120422@163.com>
2020-10-29 19:54:48 +0800

    feat: fix ci

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ const { getESLintConfig } = require('@iceworks/spec');
 module.exports = getESLintConfig('react-ts', {
   rules: {
     'no-console': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-require-imports': 'off',
     '@iceworks/best-practices/no-js-in-ts-project': 'off'
   }
